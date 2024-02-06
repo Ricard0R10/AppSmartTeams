329cf13ceec3237a1aeee18249d5f415053b307c
Ricard0R10 <143325585+Ricard0R10@users.noreply.github.com>
2024-02-06 12:37:43 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 
+const adminRoutes = require('./routes/admin');
+    app.use('/admin', adminRoutes);
+
+
 router.get('/', (req, res) => {
-    res.sendFile(__dirname + '/admin.html');
+    res.sendFile(__dirname + '/feedback.html');
 });
 
 
